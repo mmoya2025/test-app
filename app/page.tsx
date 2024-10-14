e88d7381aeb2b46a02707d3b1344407684a216d6
mariano moya <mariano@pincen.io>
2024-10-14 21:14:38 +0000

    modificacion de pagina

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-
+import {Authenticator} from '@aws-amplify/ui-react';
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
@@ -31,9 +31,11 @@ export default function App() {
     });
   }
 
+  
   return (
+    <Authenticator>
     <main>
-      <h1>My todos</h1>
+      <h1>Pedidos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -41,12 +43,13 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App de testeo login.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
       </div>
     </main>
+    </Authenticator>
   );
 }
